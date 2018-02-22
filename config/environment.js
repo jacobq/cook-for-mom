4c7076d699e6481380e11846eeceb268f0332a46
@anulman <anulman@gmail.com>
2018-02-21 21:24:36 -0500

    refactor(pods): use pods structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'cook-for-mom',
+    podModulePrefix: 'cook-for-mom/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
