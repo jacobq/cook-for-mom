58cf1329b6592f2aedc61b22ebf4dae8edef3192
@anulman <anulman@gmail.com>
2018-03-05 06:48:42 -0500

    refactor(index): update copy & styles in sync

--- app/pods/index/section-info/component.js
+++ app/pods/index/section-info/component.js
@@ -4,6 +4,7 @@ export default Component.extend({
   tagName: 'section',
   classNames: ['container-fluid', 'info'],
 
+  contentComponent: 'index/section-info/content',
   titleComponent: 'index/section-info/title',
   leadComponent: 'index/section-info/lead'
 });
--- app/pods/index/section-objection/component.js
+++ app/pods/index/section-objection/component.js
@@ -4,6 +4,7 @@ export default Component.extend({
   tagName: 'section',
   classNames: ['container-fluid', 'objection'],
 
+  childrenClass: '',
   questionComponent: 'index/section-objection/question',
   answerComponent: 'index/section-objection/answer'
 });
