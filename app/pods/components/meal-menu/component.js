b38e05007f7a0e24a855a4ca5420cdaed6b3934b
@anulman <anulman@gmail.com>
2018-03-07 03:40:03 -0500

    refactor(menu): use optimized images

--- app/pods/components/meal-menu/component.js
+++ app/pods/components/meal-menu/component.js
@@ -4,19 +4,19 @@ import { set } from '@ember/object';
 /* eslint-disable max-len */
 const DISHES = [{
   slug: 'potatoes',
-  imageUrl: '/assets/images/food/potatoes.jpg',
+  imageUrl: '/assets/images/food/potatoes-640.jpg',
   description: 'Purple potatoes with pickled cabbage and crema'
 }, {
   slug: 'soup',
-  imageUrl: '/assets/images/food/soup.jpg',
+  imageUrl: '/assets/images/food/soup-640.jpg',
   description: 'Salmorejo—a rustic Spanish tomato soup—garnished with eggs, breadcrumbs, and ham'
 }, {
   slug: 'tacos',
-  imageUrl: '/assets/images/food/tacos-1.jpg',
+  imageUrl: '/assets/images/food/tacos-1-640.jpg',
   description: 'Homemade tortilla with roasted pork shoulder or cauliflower, pickled jalapenos, jicama, cotija cheese, and pico de gallo'
 }, {
   slug: 'cookies',
-  imageUrl: '/assets/images/food/cookies.jpg',
+  imageUrl: '/assets/images/food/cookies-640.jpg',
   description: 'Bonus! Fresh-Baked Ginger Molasses Cookies'
 }];
 /* eslint-enable max-len */
