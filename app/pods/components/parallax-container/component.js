7569b6fd4752b85ef60960a7bb060549d8841b59
@anulman <anulman@gmail.com>
2018-03-10 01:01:13 -0500

    feat(parallax-container): add parallax container component

---
+++ app/pods/components/parallax-container/component.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['parallax-container'],
+
+  fluidClass: 'parallax-container-fluid',
+  fluidComponent: 'parallax-container/fluid'
+});
